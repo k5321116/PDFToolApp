2c6dabbd502323fad91d7243a2dc421e9f50682d
k5321116 <k5321116@icloud.com>
2026-04-04 07:36:41 +0900

    fix: Adjust window height to improve layout in main application

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1000, height: 750,
+    width: 1000, height: 800,
     webPreferences: { 
       nodeIntegration: true, 
       contextIsolation: false, 
